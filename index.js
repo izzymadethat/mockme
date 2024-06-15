c938529adfce2b6f72d53bc1b9469c4b4ed83df8
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-06-15 18:58:42 -0400

    update meme style layout

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const sbImage = document.getElementById("spongebob");
     const resultText = document.getElementById("result");
     const memeInput = document.getElementById("meme-input");
+    const memeResult = document.getElementById("meme-result")
 
     submitButton.addEventListener("click", (event) => {
         event.preventDefault()
@@ -10,9 +11,10 @@ document.addEventListener("DOMContentLoaded", () => {
         const userInput = memeInput.value
 
         if (userInput) {
-            const result = memeize(userInput)
-            resultText.textContent = result;
+            memeResult.style = "display:flex;"
+            resultText.textContent = memeize(userInput);
             sbImage.innerHTML = `<img src="https://th.bing.com/th/id/R.62224f3ff282bbc60bdb0bb4bc88f566?rik=CpbcekoEwPQlLw&riu=http%3a%2f%2fimages.complex.com%2fcomplex%2fimages%2fc_fill%2cg_center%2cw_1200%2ffl_lossy%2cpg_1%2cq_auto%2fbujewhyvyyg08gjksyqh%2fspongebob&ehk=4CnKxXioMpI9QHbLq9avRDsSfc3kbWocHHmIxbLNxmQ%3d&risl=&pid=ImgRaw&r=0" alt="SpongeBob Image" />`; // Add SpongeBob image to the div
+            memeInput.value = ""
         }
     })
 
